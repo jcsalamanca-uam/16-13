3623ed88ff4d66eb99c7c0f910467a807b1b633f
Julio <jcsalamanca@uamv.edu.ni>
2026-03-16 18:58:09 -0600

    Objeto creado

--- src/Main.kt
+++ src/Main.kt
@@ -23,6 +23,12 @@
               val estudiante = Estudiante()
               estudiante.nombre="Harry Joiner"
               estudiante.edad=20
+              estudiante.estudiar()
+
+              val poo = Asignatura( "POO", 250.0)
+
+              println(poo.getNombre())
+              println(poo.getPrecio())
 
 
 
